fdfd4ae1922fb2c641874e60bff9277abd4be2fb
Zahid295 <>
2023-11-12 20:48:37 +0000

    Define function to print game boards

--- run.py
+++ run.py
@@ -9,3 +9,15 @@ computer_board = []
 for i in range(5):
     player_board.append(["*"] * 5)
     computer_board.append(["*"] * 5)
+
+# Create function to print the boards
+def show_boards():
+    """
+    This function will print User and Computer Boards
+    """
+    print("User board is shown below;")
+    for row in player_board:
+        print(" ".join(row))
+    print("Computer board is shown below;")
+    for row in computer_board:
+        print(" ".join(row))
