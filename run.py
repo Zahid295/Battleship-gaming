550d31d1fed3b8947e551932334aef84838467fd
Zahid295 <>
2023-11-12 20:29:04 +0000

    Apply loop to create two game boards

--- run.py
+++ run.py
@@ -5,3 +5,7 @@ import random
 # Create 5x5 user and computer boards
 player_board = []
 computer_board = []
+# Loop to create two game boards
+for i in range(5):
+    player_board.append(["*"] * 5)
+    computer_board.append(["*"] * 5)
